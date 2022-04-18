a7fedcd4ae5beba645ec314304636cf2608c7feb
Brad Zacher <brad.zacher@gmail.com>
2022-04-18 16:45:38 -0700

    improve repro

--- file.tsx
+++ file.tsx
@@ -1,19 +1,19 @@
-export function broken(baseVersion?: string): number[] {
-  const toRelease: number[] = [];
-  const baseRelease: number[] = [];
-  return baseRelease.map((_, index) => {
-    const toPart = toRelease[index] ?? 0;
-    toPart; // this is the "working" log
-    return toPart + (baseVersion === undefined ? 0 : 1);
-  });
-}
-
 export function working(baseVersion?: string): number[] {
   const toRelease: number[] = [];
   const baseRelease: number[] = [];
   return baseRelease.map((_, index) => {
     const toPart = toRelease[index] ?? 0;
-    toPart; // this is the "broken" log
+    toPart; // this is the "working" log
     return 0;
+  });
+}
+
+export function broken(baseVersion?: string): number[] {
+  const toRelease: number[] = [];
+  const baseRelease: number[] = [];
+  return baseRelease.map((_, index) => {
+    const toPart = toRelease[index] ?? 0;
+    toPart; // this is the "broken" log
+    return toPart + (baseVersion === undefined ? 0 : 1);
   });
 }
--- repro.js
+++ repro.js
@@ -14,21 +14,25 @@ const program = ts.createProgram({
 const sourceFile = program.getSourceFile(testFilePath);
 const checker = program.getTypeChecker();
 
+
+function getIdentifier(statement) {
+  return statement
+    .body
+    .statements[2]
+    .expression
+    .arguments[0]
+    .body
+    .statements[1]
+    .expression
+}
+
+
 ////
 //// WORKING CASE
 ////
 
-// this will pick the `toPart` identifier on line 22
-const nodeWorking = sourceFile
-  .statements[1]
-  // @ts-expect-error -- too lazy to refine properly
-  .body
-  .statements[2]
-  .expression
-  .arguments[0]
-  .body
-  .statements[1]
-  .expression;
+// this will pick the `toPart` identifier on line 6
+const nodeWorking = getIdentifier(sourceFile.statements[0]);
 const typeWorking = checker.getTypeAtLocation(nodeWorking);
 console.log(
   'working case:',
@@ -41,17 +45,8 @@ console.log(
 //// BROKEN CASE
 ////
 
-// this will pick the `toPart` identifier on line 10
-const nodeBroken = sourceFile
-  .statements[0]
-  // @ts-expect-error -- too lazy to refine properly
-  .body
-  .statements[2]
-  .expression
-  .arguments[0]
-  .body
-  .statements[1]
-  .expression;
+// this will pick the `toPart` identifier on line 16
+const nodeBroken = getIdentifier(sourceFile.statements[1]);
 const typeBroken = checker.getTypeAtLocation(nodeBroken);
 // it *should* print `number`, but it prints `any`
 console.log(
